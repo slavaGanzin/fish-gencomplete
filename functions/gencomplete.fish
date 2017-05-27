7acdeb05c50cd77a4263defb6520960b754c1b76
slavaGanzin <slava.ganzin@gmail.com>
2017-05-28 00:10:50 +0300

    save completions in ~/.config/fish

--- functions/gencomplete.fish
+++ functions/gencomplete.fish
@@ -1,5 +1,5 @@
-function gencomplete -d "Generate fish completions using command's --help" --argument-names 'name' 'help'
-  if test -z $name
+function gencomplete --description Generate\ fish\ completions\ using\ command\'s\ --help --argument name help
+	if test -z $name
     echo Usage: gencomplete binaryInPath [--help-key-of-binary]
     return 127
   end
@@ -10,7 +10,7 @@ function gencomplete -d "Generate fish completions using command's --help" --arg
   set -l mp /tmp/man/$name.1
   help2man $name -h $help -o $mp
 
-  python -B $__fish_datadir/tools/create_manpage_completions.py --progress /tmp/man/$name $mp
+  python -B $__fish_datadir/tools/create_manpage_completions.py --progress /tmp/man/$name $mp --directory ~/.config/fish/completions
 
   rm $mp
 end
