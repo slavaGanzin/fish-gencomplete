ec47d2161fd1d9cf7362971f60cb798528712d60
slavaGanzin <slava.ganzin@gmail.com>
2017-05-28 14:03:50 +0300

    auto source completion

--- functions/gencomplete.fish
+++ functions/gencomplete.fish
@@ -1,4 +1,6 @@
 function gencomplete --description Generate\ fish\ completions\ using\ command\'s\ --help --argument name help
+  set -l compdir ~/.config/fish/completions
+
 	if test -z $name
     echo Usage: gencomplete binaryInPath [--help-key-of-binary]
     return 127
@@ -10,7 +12,9 @@ function gencomplete --description Generate\ fish\ completions\ using\ command\'
   set -l mp /tmp/man/$name.1
   help2man $name -h $help -o $mp
 
-  python -B $__fish_datadir/tools/create_manpage_completions.py --progress /tmp/man/$name $mp --directory ~/.config/fish/completions
+  python -B $__fish_datadir/tools/create_manpage_completions.py --progress $mp --directory $compdir
+
+  source $compdir/$name.fish
 
   rm $mp
 end
