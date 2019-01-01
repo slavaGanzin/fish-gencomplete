4c4e55fb7a29880c2348131a18f992613e248be8
Slava Ganzin <slava.ganzin@gmail.com>
2019-01-01 20:49:05 +0200

    fix generation

--- functions/gencomplete.fish
+++ functions/gencomplete.fish
@@ -10,7 +10,14 @@ function gencomplete --description "Generate fish completions using command's --
 
   mkdir /tmp/man ^/dev/null
   set -l mp /tmp/man/$name.1
-  help2man $name -h "$help" -o $mp
+
+  man_path=(man -Ww ls)
+
+  if test -z "$man_path"
+    help2man $name -h "$help" -o $mp --no-discard-stderr
+  else
+    cat man_path | gunzip > $mp
+  end
 
   python -B $__fish_datadir/tools/create_manpage_completions.py --progress $mp --directory $compdir
 
